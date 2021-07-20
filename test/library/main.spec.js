49e5ceac2b1c0d3307ee5b86c17d4ee33e0a161e
RickChen <xtforgame@gmail.com>
2021-07-21 06:14:24 +0800

    Xxx

--- test/library/main.spec.js
+++ test/library/main.spec.js
@@ -11,13 +11,6 @@ import {
 const { expect } = chai;
 
 describe('Main Test Cases', () => {
-  describe('Basic', () => {
-    it('mainFunc should be a function', () => {
-      expect(mainFunc).to.be.an.instanceof(Function);
-      return true;
-    });
-  });
-
   describe('Echo Test', function () {
     this.timeout(30000000);
     it('.then()', async () => {
