0d5c0f9ff4013008b927b35812d2e5b699d943b5
RickChen <xtforgame@gmail.com>
2021-07-20 15:54:01 +0800

    Xxxxx

--- src/library/utils/index.ts
+++ src/library/utils/index.ts
@@ -22,3 +22,15 @@ export function promiseReduce<T>(
 ) {
   return inArray.reduce((prev, curr, index, array) => prev.then(v => toPrmiseFunc(v, curr, index, array)), Promise.resolve(startValue));
 }
+
+
+export function toMap<T1, T2 = T1>(
+  inArray : T1[],
+  getId : (t: T1, i: number, array: T1[]) => any,
+  trans : (t: T1, i: number, array: T1[]) => T2 = (t => <T2><any>t),
+) {
+  return inArray.reduce((prev, curr, index, array) => {
+    prev[getId(curr, index, array)] = trans(curr, index, array);
+    return prev;
+  }, <{ [s: string]: T2 }>{});
+}
