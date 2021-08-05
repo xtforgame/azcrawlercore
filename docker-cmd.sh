79fa9ab58e0c5c9d85883f787f9f09e063fcc1d8
RickChen <xtforgame@gmail.com>
2021-08-05 19:17:33 +0800

    Xxxxx

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -2,7 +2,9 @@
 mkdir -p /usr/volumes/src
 cp -rf /usr/volumes/src/* /usr/src/app
 
-#npm start
-npm run build
-tail -f /dev/null
-# node dist/server/index.js
+npm run test
+
+# #npm start
+# npm run build
+# tail -f /dev/null
+# # node dist/server/index.js
