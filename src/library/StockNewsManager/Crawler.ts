a6c9ccd2ef059f1786b6e45dc04508ce15cf8a86
RickChen <xtforgame@gmail.com>
2021-08-09 06:55:01 +0800

    Xxxx

--- src/library/StockNewsManager/Crawler.ts
+++ src/library/StockNewsManager/Crawler.ts
@@ -200,7 +200,11 @@ export default class Crawler {
   }
 
   async fetch() {
-    await fs.remove(`${process.env.APIFY_LOCAL_STORAGE_DIR}`);
+    try {
+      await fs.remove(`${process.env.APIFY_LOCAL_STORAGE_DIR}`);
+    } catch (error) {
+      
+    }
     const proxyConfiguration = await Apify.createProxyConfiguration({
       proxyUrls: ['http://spe19aeb13:XEdSfouN79@gate.dc.smartproxy.com:20000'],
     });
