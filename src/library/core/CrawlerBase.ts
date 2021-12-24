a5488e57632b5d2975dbff2b5be81af2b9f87cdf
RickChen <xtforgame@gmail.com>
2021-12-24 17:08:57 +0800

    temp

--- src/library/core/CrawlerBase.ts
+++ src/library/core/CrawlerBase.ts
@@ -3,11 +3,46 @@ import path from 'path';
 import moment from 'moment';
 import puppeteer, { launch, Browser } from 'puppeteer';
 import useProxy from 'puppeteer-page-proxy';
-import { promiseWait, promiseWaitFor } from '~/utils';
+import { promiseReduce, promiseWait, promiseWaitFor } from '~/utils';
 
 export type PuppeteerLaunchOptions = Parameters<typeof launch>[0];
 
+
+type Exchange = {
+  id: string;
+  name: string;
+};
+
+const exchanges : Exchange[] = [
+  { id: 'amex', name: 'AMEX' },
+  { id: 'nasd', name: 'NASDAQ' },
+  { id: 'nyse', name: 'NYSE' },
+];
+
+export class DataStore {
+  filepath: string;
+
+  constructor(filepath: string) {
+    this.filepath = filepath;
+  }
+
+  setValue(path: string, data: any) {
+
+  }
+}
+
 export default class CrawlerBase {
+  symbolStore: DataStore;
+  resultStore: DataStore;
+
+  constructor() {
+    this.symbolStore = new DataStore('xxxxx');
+    this.resultStore = new DataStore('result');
+  }
+
+  getExchangeStartUrl(exchange : Exchange) {
+    return `https://finviz.com/screener.ashx?v=111&f=exch_${exchange.id}`;
+  }
 
   getPuppeteerLaunchOptions(debug : boolean = false) : PuppeteerLaunchOptions {
     const args = [
@@ -59,41 +94,134 @@ export default class CrawlerBase {
     return page;
   }
 
-  async run() {
+  async handleQuoteResult(page: puppeteer.Page, exchange: Exchange, url: string, symbol: string) {
+    const data = await page.$$eval('.fullview-profile', ($profile: Element[]) => {
+      return {
+        url: '',
+        scrapedData: $profile?.[0]?.innerText || '',
+      };
+    });
+
+    data.url = url;
+
+    await this.resultStore.setValue(symbol, data);
+
+    // console.log('data :', data);
+  }
+
+  async handleListResult(page: puppeteer.Page, exchange: Exchange, url: string) {
+    // A function to be evaluated by Puppeteer within the browser context.
+    const data = await page.$$eval('#screener-content tbody tr:nth-child(4) tbody', ($tbody) => {
+      const scrapedData : any[] = [];
+
+      const $etfs : any[] = Array.from($tbody[0].querySelectorAll('tr'));
+      const $header = $etfs.shift();
+      const $columns : any[] = Array.from($header.querySelectorAll('td'));
+      // We're getting the title, rank and URL of each post on Hacker News.
+      $etfs.forEach(($etf) => {
+        const record = {};
+        const a : any[] = Array.from($etf.querySelectorAll('td'));
+        a.forEach((cell, i) => {
+          record[$columns[i].innerText] = cell.innerText;
+        });
+        scrapedData.push(record);
+      });
+
+      const tabLinks = document.querySelectorAll('.screener_pagination .tab-link');
+      // console.log('x :', x);
+      // debugger;
+      return {
+        scrapedData,
+        tabLinks: tabLinks.length,
+      };
+    });
+
+    console.log('data :', data);
+
+    // Store the results to the default dataset.
+    await promiseReduce<any>(data.scrapedData, async (_, d) => {
+      await this.symbolStore.setValue(d.Ticker, {
+        ...d,
+        exchange,
+      });
+
+      await this.newBrowser(`https://finviz.com/quote.ashx?t=${d.Ticker}`, {}, async ({
+        page, url,
+      }) => {
+        await this.handleQuoteResult(page, exchange, url, d.Ticker);
+      });
+
+      // const req = await requestQueue.addRequest({ url: `https://finviz.com/quote.ashx?t=${d.Ticker}` });
+      // await requestStore.setValue(req.requestId, {
+      //   type: 'quote',
+      //   symbol: d.Ticker,
+      // });
+    }, <any>null);
+    // await Apify.pushData(data);
+
+    // // Find a link to the next page and enqueue it if it exists.
+    // const infos = await Apify.utils.enqueueLinks({
+    //   page,
+    //   requestQueue,
+    //   selector: '.screener_pagination a.tab-link:last-child',
+    // });
+    // await promiseReduce<any>(infos, async (_, info) => {
+    //   await requestStore.setValue(info.requestId, {
+    //     type: 'list',
+    //   });
+    // }, <any>null);
+
+    // if (data.tabLinks === 2 && this.getCurrentUrl() !== request.url) {
+    //   this.fetchCounter++;
+    //   if (this.fetchCounter < exchanges.length) {
+    //     const req = await requestQueue.addRequest({ url: this.getCurrentUrl() });
+    //     await requestStore.setValue(req.requestId, {
+    //       type: 'list',
+    //     });
+    //   } else {
+    //     console.log(`${request.url} is the last page!`);
+    //     await this.onDone();
+    //   }
+    // }
+    // // if (infos.length === 0) {
+    // // }
+  }
+
+  async newBrowser<Args = any>(url: string, args: Args, p: (params: {
+    browser: puppeteer.Browser;
+    page: puppeteer.Page;
+    url: string;
+    args: Args;
+  }) => Promise<any>) {
     const browser = await puppeteer.launch(this.getPuppeteerLaunchOptions(true));
     const page = await this.newPage(browser);
-    // await useProxy(page, 'http://127.0.0.1:80');
+    await useProxy(page, 'http://spe19aeb13:XEdSfouN79@gate.dc.smartproxy.com:20000');
     // console.log('page');
-    const p = new Promise((resolve, reject) => {
-      let fulfill = false;
-      page.on('response', (resp) => {
-        const url = resp.url();
-        if (!fulfill && url.includes('spec.json')) {
-          fulfill = true;
-          resp.json().then(resolve);
-        }
-      });
-      setTimeout(() => {
-        if (!fulfill) {
-          fulfill = true;
-          reject(new Error('Expired'));
-        }
-      }, 15000);
-    });
-    await page.goto('https://httpbin.org', {
+    await page.goto(url, {
       waitUntil: 'networkidle2',
     });
     const session = await page.target().createCDPSession();
     await session.send('Page.enable');
-    // await page.screenshot({ path: 'example.png' });
-    const json = await p;
-    console.log('json :', json);
-    const title = await page.$$eval('hgroup h2.title', ($div) => {
-      debugger;
-      return $div?.[0]?.innerHTML || '';
-    });
-    console.log('title :', title);
+    try {
+      await p({ browser, page, url, args });
+    } catch (error) {
+      console.log('error :', error);
+    }
     await browser.close();
+  }
+
+  async fetchExchange(exchange: Exchange) {
+    await this.newBrowser(this.getExchangeStartUrl(exchange), {}, async ({
+      page, url,
+    }) => {
+      await this.handleListResult(page, exchange, url);
+    });
     return 1;
+  }
+
+  async run() {
+    await promiseReduce(exchanges, async (_, exchange) => {
+      await this.fetchExchange(exchange);
+    }, null);
   }
 }
